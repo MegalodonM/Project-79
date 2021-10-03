3db37472d4230cf31dd24c378e5fd71874d4567f
MegalodonM <91394469+MegalodonM@users.noreply.github.com>
2021-10-03 15:42:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+menu_list_array = ["Veg Margherita Pizza","Cheese Pizza", "Chicken Pizza","Desi Pizza","Paneer Tika Pizza","Veggie Deluxe Pizza"];
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        htmldata=""
+        for(var i=0;i<menu_list_array.sort;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
